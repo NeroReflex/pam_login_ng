1a040c5bcb5de14ac61e27f134d138080cb11dbd
Denis Benato <benato.denis96@gmail.com>
2024-11-23 17:27:29 +0100

    GPL 2 License

--- src/login-ng_cli/main.rs
+++ src/login-ng_cli/main.rs
@@ -1,3 +1,22 @@
+/*
+    login-ng A greeter written in rust that also supports autologin with systemd-homed
+    Copyright (C) 2024  Denis Benato
+    
+    This program is free software; you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; either version 2 of the License, or
+    (at your option) any later version.
+    
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+    You should have received a copy of the GNU General Public License along
+    with this program; if not, write to the Free Software Foundation, Inc.,
+    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+*/
+
 use std::env;
 use std::sync::Arc;
 use std::sync::Mutex;
@@ -60,6 +79,12 @@ fn login_pam(
 }
 
 fn main() {
+    println!("login-ng version 0.1.0, Copyright (C) 2024 Denis Benato");
+    println!("login-ng comes with ABSOLUTELY NO WARRANTY;");
+    println!("This is free software, and you are welcome to redistribute it");
+    println!("under certain conditions.");
+    println!("");
+
     let args: Args = argh::from_env();
 
     let allow_autologin = args.autologin.unwrap_or(false);
