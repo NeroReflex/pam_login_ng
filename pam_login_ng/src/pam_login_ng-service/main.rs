57cb22f0d6402db0633ffed1aaed871c80e57bce
Denis Benato <benato.denis96@gmail.com>
2025-03-24 16:45:26 +0000

    Simplify code

--- pam_login_ng/src/pam_login_ng-service/main.rs
+++ pam_login_ng/src/pam_login_ng-service/main.rs
@@ -21,20 +21,20 @@ extern crate rand;
 extern crate sys_mount;
 extern crate tokio;
 
+use tokio::signal::unix::{signal, SignalKind};
+use tokio::sync::Mutex;
+
 use sys_mount::{Mount, MountFlags, SupportedFilesystems, Unmount, UnmountDrop, UnmountFlags};
 
 use login_ng::{
+    mount::MountPoints,
     storage::{load_user_auth_data, load_user_mountpoints},
     users::{self, get_user_by_name, os::unix::UserExt},
 };
 
-use thiserror::Error;
-
 use std::{collections::HashMap, ffi::OsString, io};
 use std::{fs::create_dir, path::Path, sync::Arc};
-use tokio::sync::Mutex;
-
-use std::future::pending;
+use thiserror::Error;
 use zbus::{connection, interface, Error as ZError};
 
 use std::fs::File;
@@ -83,6 +83,44 @@ impl Service {
     }
 }
 
+/// Mounts a filesystem at the specified path.
+///
+/// This function takes a tuple containing information necessary for mounting a filesystem.
+/// It checks if the specified mount path exists and is a directory. If the path does not exist,
+/// it attempts to create it. Depending on whether the filesystem type is provided, it constructs
+/// a mount operation accordingly.
+///
+/// # Parameters
+///
+/// - `data`: A tuple of four `String` values:
+///   - `data.0`: The filesystem type (e.g., "ext4", "nfs"). If this is an empty string, the mount
+///     operation will be performed without specifying a filesystem type.
+///   - `data.1`: Additional data required for the mount operation (e.g., options for the mount).
+///   - `data.2`: The source of the filesystem to mount (e.g., a device or remote filesystem).
+///   - `data.3`: The target directory where the filesystem should be mounted.
+///
+/// # Returns
+///
+/// Returns a `Result<Mount, io::Error>`. On success, it returns a `Mount` object representing
+/// the mounted filesystem. On failure, it returns an `io::Error` indicating what went wrong,
+/// which could include issues with directory creation or mounting the filesystem.
+///
+/// # Errors
+///
+/// This function may fail if:
+/// - The specified mount path does not exist and cannot be created due to permission issues.
+/// - The mount operation fails due to invalid parameters or system errors.
+///
+/// # Example
+///
+/// ```rust
+/// let mount_data = (String::from("ext4"), String::from("rw"), String::from("/dev/sda1"), String::from("/mnt/my_mount"));
+/// match mount(mount_data) {
+///     Ok(mount) => println!("Mounted successfully: {:?}", mount),
+///     Err(e) => eprintln!("Failed to mount: {}", e),
+/// }
+/// ```
+///
 fn mount(data: (String, String, String, String)) -> io::Result<Mount> {
     let mount_path = Path::new(data.3.as_str());
     if !mount_path.exists() || !mount_path.is_dir() {
@@ -91,12 +129,74 @@ fn mount(data: (String, String, String, String)) -> io::Result<Mount> {
     }
 
     match data.0.is_empty() {
-        true => Mount::builder().mount(data.2.as_str(), data.3.as_str()),
+        true => Mount::builder().mount(data.2.as_str(), mount_path.as_os_str()),
         false => Mount::builder()
             .fstype(data.0.as_str())
             .data(data.1.as_str())
             .mount(data.2.as_str(), data.3.as_str()),
     }
+}
+
+fn mount_all(mounts: MountPoints, username: String, homedir: String) -> Vec<UnmountDrop<Mount>> {
+    let mut mounted_devices = vec![];
+
+    for m in mounts
+        .foreach(|a, b| {
+            (
+                b.fstype().clone(),
+                b.flags().join(",").clone(),
+                b.device().clone(),
+                a.clone(),
+            )
+        })
+        .iter()
+    {
+        match mount(m.clone()) {
+            Ok(mount) => {
+                println!(
+                    "Mounted device {} into {} for user '{username}'",
+                    m.2.as_str(),
+                    m.3.as_str(),
+                );
+
+                // Make the mount temporary, so that it will be unmounted on drop.
+                mounted_devices.push(mount.into_unmount_drop(UnmountFlags::DETACH));
+            }
+            Err(err) => {
+                eprintln!(
+                    "failed to mount device {} into {}: {}",
+                    m.2.as_str(),
+                    m.3.as_str(),
+                    err
+                );
+
+                return vec![];
+            }
+        }
+    }
+
+    match mount((
+        mounts.mount().fstype().clone(),
+        mounts.mount().flags().join(","),
+        mounts.mount().device().clone(),
+        homedir,
+    )) {
+        Ok(mount) => {
+            println!(
+                "Mounted device {} on home directory for user '{username}'",
+                mounts.mount().device().as_str(),
+            );
+
+            // Make the mount temporary, so that it will be unmounted on drop.
+            mounted_devices.push(mount.into_unmount_drop(UnmountFlags::DETACH));
+        }
+        Err(err) => {
+            eprintln!("failed to mount user directory: {err}");
+            return vec![];
+        }
+    }
+
+    mounted_devices
 }
 
 #[interface(name = "org.zbus.login_ng1")]
@@ -115,6 +215,11 @@ impl Service {
         println!("Requested session for user '{user}' to be opened");
 
         let source = login_ng::storage::StorageSource::Username(String::from(user));
+
+        let Some(user) = get_user_by_name(user) else {
+            // cannot identify user
+            return 7u32;
+        };
 
         let password = match self.priv_key.decrypt(Pkcs1v15Encrypt, &password) {
             Ok(password) => {
@@ -136,92 +241,46 @@ impl Service {
             }
         };
 
-        println!("Mountpoints for user '{user}' loaded");
-
         // TODO: check for the mount to be approved by root
         // otherwise the user might mount everything he wants to
         // with every dmask, potentially compromising the
         // security and integrity of the whole system.
 
-        let Some(user) = get_user_by_name(user) else {
-            // cannot identify user
-            return 7u32;
-        };
-
-        let mut mounted_devices = vec![];
-
         // mount every directory in order or throw an error
-        if let Some(mounts) = user_mounts {
-            let staged_mounts = mounts.foreach(|a, b| {
-                (
-                    b.fstype().clone(),
-                    b.flags().join(",").clone(),
-                    b.device().clone(),
-                    a.clone(),
-                )
-            });
+        let mounted_devices = match user_mounts {
+            Some(mounts) => {
+                let mounted_devices = mount_all(
+                    mounts,
+                    user.name().to_string_lossy().to_string(),
+                    user.home_dir().as_os_str().to_string_lossy().to_string(),
+                );
 
-            for m in staged_mounts.iter() {
-                match mount(m.clone()) {
-                    Ok(mount) => {
-                        println!(
-                            "Mounted device {} into {} for user '{}'",
-                            m.2.as_str(),
-                            m.3.as_str(),
-                            user.name().to_string_lossy()
-                        );
-
-                        // Make the mount temporary, so that it will be unmounted on drop.
-                        mounted_devices.push(mount.into_unmount_drop(UnmountFlags::DETACH));
-                    }
-                    Err(err) => {
-                        eprintln!(
-                            "failed to mount device {} into {}: {}",
-                            m.2.as_str(),
-                            m.3.as_str(),
-                            err
-                        );
-                        return 4u32;
-                    }
-                }
-            }
-
-            match mount((
-                mounts.mount().fstype().clone(),
-                mounts.mount().flags().join(","),
-                mounts.mount().device().clone(),
-                user.home_dir().as_os_str().to_string_lossy().to_string(),
-            )) {
-                Ok(mount) => {
-                    println!(
-                        "Mounted device {} on home directory for user '{}'",
-                        mounts.mount().device().as_str(),
+                if mounted_devices.is_empty() {
+                    eprintln!(
+                        "Failed to mount one or more devices for user '{}'",
                         user.name().to_string_lossy()
                     );
 
-                    // Make the mount temporary, so that it will be unmounted on drop.
-                    mounted_devices.push(mount.into_unmount_drop(UnmountFlags::DETACH));
-                }
-                Err(err) => {
-                    eprintln!("failed to mount user directory: {err}");
                     return 4u32;
                 }
-            }
-        }
 
-        println!(
-            "Successfilly mounted {} device for user '{}'",
-            mounted_devices.len(),
-            user.name().to_string_lossy()
-        );
+                println!(
+                    "Successfilly mounted {} device for user '{}'",
+                    mounted_devices.len(),
+                    user.name().to_string_lossy()
+                );
+
+                mounted_devices
+            }
+            None => vec![],
+        };
+
+        let user_session = UserSession {
+            mounts: mounted_devices,
+        };
 
         let mut guard = self.sessions.lock().await;
-        guard.insert(
-            user.name().to_os_string(),
-            UserSession {
-                mounts: mounted_devices,
-            },
-        );
+        guard.insert(user.name().to_os_string(), user_session);
 
         println!(
             "Successfilly opened session for user '{}'",
@@ -242,16 +301,16 @@ impl Service {
         let username = user.name().to_string_lossy();
 
         let mut guard = self.sessions.lock().await;
-        if !guard.contains_key(user.name()) {
-            // session already closed
-            return 6u32;
-        }
 
-        // due to how directories are mounted discarding the session also umounts all mount points
-        let session = guard.remove(user.name());
-        drop(session);
+        // due to how directories are mounted discarding the session also umounts all mount points:
+        // either remove the user session from the collection and destroy the session or
+        // report to the caller that the requested session is already closed
+        match guard.remove(user.name()) {
+            Some(user_session) => drop(user_session),
+            None => return 6u32,
+        };
 
-        println!("Successfilly closed session for user '{username}'");
+        println!("Successfully closed session for user '{username}'");
 
         0u32
     }
@@ -271,8 +330,6 @@ async fn main() -> Result<(), ServiceError> {
         return Err(ServiceError::MissingPrivilegesError);
     }
 
-    let service = Service::new(contents.as_str());
-
     match std::env::var("DBUS_SESSION_BUS_ADDRESS") {
         Ok(value) => println!("Starting dbus service on socket {value}"),
         Err(err) => {
@@ -286,11 +343,11 @@ async fn main() -> Result<(), ServiceError> {
 
     println!("Building the dbus object...");
 
-    let _conn = connection::Builder::session()
+    let dbus_conn = connection::Builder::session()
         .map_err(|err| ServiceError::ZbusError(err))?
         .name("org.zbus.login_ng")
         .map_err(|err| ServiceError::ZbusError(err))?
-        .serve_at("/org/zbus/login_ng", service)
+        .serve_at("/org/zbus/login_ng", Service::new(contents.as_str()))
         .map_err(|err| ServiceError::ZbusError(err))?
         .build()
         .await
@@ -298,8 +355,12 @@ async fn main() -> Result<(), ServiceError> {
 
     println!("Application running");
 
-    // Do other things or go to wait forever
-    pending::<()>().await;
+    // Create a signal listener for SIGTERM
+    let mut sigterm =
+        signal(SignalKind::terminate()).expect("Failed to create SIGTERM signal handler");
 
-    Ok(())
+    // Wait for a SIGTERM signal
+    sigterm.recv().await;
+
+    Ok(drop(dbus_conn))
 }
