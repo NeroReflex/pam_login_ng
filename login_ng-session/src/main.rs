034f40cd84ce175307f78e5087b6d18e1015ced3
Denis Benato <benato.denis96@gmail.com>
2025-04-14 15:52:24 +0000

    More verbose output for login_ng-session

--- login_ng-session/src/main.rs
+++ login_ng-session/src/main.rs
@@ -65,10 +65,14 @@ async fn main() -> Result<(), SessionManagerError> {
             login_ng_session::errors::NodeLoadingError::FileNotFound(filename) => {
                 // if the default target is missing use the default user shell
                 if filename == default_service_name {
+                    let shell = user.clone().shell.to_string_lossy().into_owned();
+                    
+                    eprintln!("Definition for {default_service_name} not found: using shell {shell}");
+
                     nodes = HashMap::from([(
                         default_service_name.clone(),
                         Arc::new(RwLock::new(SessionNode::new(
-                            user.clone().shell.to_string_lossy().into_owned(),
+                            shell.clone(),
                             &[],
                             nix::sys::signal::Signal::SIGINT,
                             SessionNodeRestart::no_restart(),
