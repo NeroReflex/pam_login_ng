a8bd35d92c8486126bc5ddb3545f76697e26b711
Denis Benato <benato.denis96@gmail.com>
2025-05-08 03:40:29 +0200

    Put some placeholder code

--- login_ng-gui/src/main.rs
+++ login_ng-gui/src/main.rs
@@ -3,11 +3,74 @@
 
 use std::{env, error::Error, ffi::OsString, sync::{Arc, Mutex}};
 
-use login_ng::users::os::unix::UserExt;
-use login_ng_user_interactions::{cli::CommandLineLoginUserInteractionHandler, login::{LoginExecutor, SessionCommandRetrival}};
+use login_ng::{storage::{load_user_auth_data, StorageSource}, user::UserAuthData, users::os::unix::UserExt};
+use login_ng_user_interactions::login::{LoginExecutor, LoginUserInteractionHandler, SessionCommandRetrival};
 use slint::ModelRc;
 
 slint::include_modules!();
+
+#[derive(Default)]
+pub struct GUILoginUserInteractionHandler {
+    attempt_autologin: bool,
+
+    maybe_user: Option<UserAuthData>,
+
+    maybe_username: Option<String>,
+}
+
+impl GUILoginUserInteractionHandler {
+    pub fn new(
+        attempt_autologin: bool,
+        maybe_username: Option<String>,
+    ) -> Self {
+        let maybe_user = match &maybe_username {
+            Some(username) => {
+                load_user_auth_data(&StorageSource::Username(username.clone())).map_or(None, |a| a)
+            }
+            None => None,
+        };
+
+        Self {
+            attempt_autologin,
+            maybe_user,
+            maybe_username,
+        }
+    }
+}
+
+impl LoginUserInteractionHandler for GUILoginUserInteractionHandler {
+    fn provide_username(&mut self, username: &String) {
+        self.maybe_user =
+            load_user_auth_data(&StorageSource::Username(username.clone())).map_or(None, |a| a)
+    }
+
+    fn prompt_secret(&mut self, msg: &String) -> Option<String> {
+        if self.attempt_autologin {
+            if let Some(user_cfg) = &self.maybe_user {
+                if let Ok(main_password) = user_cfg.main_by_auth(&Some(String::new())) {
+                    return Some(main_password);
+                }
+            }
+        }
+
+        todo!()
+    }
+
+    fn prompt_plain(&mut self, msg: &String) -> Option<String> {
+        match &self.maybe_username {
+            Some(username) => Some(username.clone()),
+            None => todo!(),
+        }
+    }
+
+    fn print_info(&mut self, msg: &String) {
+        println!("{}", msg)
+    }
+
+    fn print_error(&mut self, msg: &String) {
+        eprintln!("{}", msg)
+    }
+}
 
 fn main() -> Result<(), Box<dyn Error>> {
     let ui = AppWindow::new()?;
@@ -39,14 +102,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let ui_handle = ui.as_weak();
     ui.on_request_login(move |username| {
-        let _ui = ui_handle.unwrap();
+        let ui = ui_handle.unwrap();
 
         let maybe_username = Some(username.as_str().to_string());
 
-        let prompter = Arc::new(Mutex::new(CommandLineLoginUserInteractionHandler::new(
+        let prompter = Arc::new(Mutex::new(GUILoginUserInteractionHandler::new(
             true,
             Some(username.as_str().to_string()),
-            None,
         )));
 
         use login_ng_user_interactions::greetd::GreetdLoginExecutor;
@@ -55,7 +117,6 @@ fn main() -> Result<(), Box<dyn Error>> {
 
         login_executor.execute(&maybe_username, &SessionCommandRetrival::AutodetectFromUserHome).unwrap();
 
-        //let ui_handle = ui.as_weak();
         /*move || {
             let ui = ui_handle.unwrap();
             let selected_user = ui.get_selectedUser();
