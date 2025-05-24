99bfbec37f63254dcbc57d25b41d9c12357f67e3
Denis Benato <benato.denis96@gmail.com>
2025-05-24 17:24:08 +0200

    Implement session switching

--- sessionexec/src/main.rs
+++ sessionexec/src/main.rs
@@ -1,12 +1,10 @@
 use ini::Ini;
 use std::error::Error;
 use std::process::Command;
-use std::{env, ffi::CString, path::PathBuf, ptr::null};
+use std::{env, ffi::CString, path::PathBuf};
 
 fn find_program_path(program: &str) -> Result<String, Box<dyn Error>> {
-    let output = Command::new("which")
-        .arg(program)
-        .output()?;
+    let output = Command::new("which").arg(program).output()?;
 
     if output.status.success() {
         let path = String::from_utf8_lossy(&output.stdout).trim().to_string();
@@ -19,11 +17,30 @@ fn find_program_path(program: &str) -> Result<String, Box<dyn Error>> {
 fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
 
-    if args.len() < 2 {
-        panic!("No session specified")
-    }
-
-    let param = args[1].to_string();
+    let param = if args.len() < 2 {
+        match std::env::home_dir() {
+            Some(home_dir) => {
+                let path = home_dir.join(".config").join("sessionexec").join("default");
+                if path.exists() && path.is_file() {
+                    match std::fs::read_to_string(path) {
+                        Ok(session) => session.trim().to_string(),
+                        Err(err) => {
+                            println!("error reading file ~/.config/sessionexec/default: {err}");
+                            String::from("game-mode.desktop")
+                        }
+                    }
+                } else {
+                    println!(
+                        "file ~/.config/sessionexec/default not found: using game-mode.desktop"
+                    );
+                    String::from("game-mode.desktop")
+                }
+            }
+            None => String::from("game-mode.desktop"),
+        }
+    } else {
+        args[1].to_string()
+    };
 
     let path = PathBuf::from("/usr/share/wayland-sessions/").join(param);
 
@@ -61,7 +78,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         argv_data.push(c_string);
     }
 
-    let argv = argv_data.iter().map(|e| e.as_ptr()).chain(std::iter::once(std::ptr::null())).collect::<Vec<*const libc::c_char>>();
+    let argv = argv_data
+        .iter()
+        .map(|e| e.as_ptr())
+        .chain(std::iter::once(std::ptr::null()))
+        .collect::<Vec<*const libc::c_char>>();
 
     let mut envp_data: Vec<CString> = vec![];
     for (idx, (key, value)) in env::vars().enumerate() {
@@ -71,7 +92,11 @@ fn main() -> Result<(), Box<dyn Error>> {
         envp_data.push(c_string);
     }
 
-    let envp = envp_data.iter().map(|e| e.as_ptr()).chain(std::iter::once(std::ptr::null())).collect::<Vec<*const libc::c_char>>();
+    let envp = envp_data
+        .iter()
+        .map(|e| e.as_ptr())
+        .chain(std::iter::once(std::ptr::null()))
+        .collect::<Vec<*const libc::c_char>>();
 
     let execve_err = unsafe { libc::execve(prog.as_ptr(), argv.as_ptr(), envp.as_ptr()) };
 
@@ -79,5 +104,5 @@ fn main() -> Result<(), Box<dyn Error>> {
         return Err(format!("execve failed: {}", std::io::Error::last_os_error()).into());
     }
 
-    Ok(())
+    unreachable!()
 }
