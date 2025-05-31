90fac78a13b7f934bbc38d3c4c0943d199bb03bf
Denis Benato <benato.denis96@gmail.com>
2025-05-31 19:12:23 +0200

    do not free memory

--- sessionexec/src/cstr.rs
+++ sessionexec/src/cstr.rs
@@ -6,9 +6,11 @@ pub struct CStr {
 
 impl Drop for CStr {
     fn drop(&mut self) {
+        /*
         if !self.data.is_null() {
             unsafe { libc::free(self.data as *mut libc::c_void) }
         }
+        */
     }
 }
 
