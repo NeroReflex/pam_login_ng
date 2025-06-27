e79fdef09498a7c3afd36129fbbbe734822e8696
Denis Benato <benato.denis96@gmail.com>
2025-06-27 19:45:13 +0200

    Improve make_release script

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
 new_version=${1}
 version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' login_ng/Cargo.toml)
-find . -name "*.toml"  -type f -exec sed -i "s/$version/$new_version/g" {} +
+find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
+sed -i "s/\"$version\"/\"$new_version\"/g" Cargo.lock
 git add .
 git commit -m "Bump version to $new_version"
 git tag $new_version
