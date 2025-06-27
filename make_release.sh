4e4d9364eb7b45928da8e135edc85adcac2ad3bf
Denis Benato <benato.denis96@gmail.com>
2025-06-27 19:42:58 +0200

    Create a new commit before tagging

--- make_release.sh
+++ make_release.sh
@@ -1,4 +1,6 @@
 new_version=${1}
 version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' login_ng/Cargo.toml)
 find . -name "*.toml"  -type f -exec sed -i "s/$version/$new_version/g" {} +
+git add .
+git commit -m "Bump version to $new_version"
 git tag $new_version
