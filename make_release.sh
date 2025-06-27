f5f27547e08a900e43551a4a4f8d2a7cfa1d5d73
Denis Benato <benato.denis96@gmail.com>
2025-06-27 19:51:09 +0200

    faster build

--- make_release.sh
+++ make_release.sh
@@ -3,7 +3,7 @@ new_version=${1}
 version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' login_ng/Cargo.toml)
 find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
 #sed -i "s/\"$version\"/\"$new_version\"/g" Cargo.lock
-cargo build --all-features
+cargo build
 git add .
 git commit -m "Bump version to $new_version"
 git tag $new_version
