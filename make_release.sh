116339809b5b3381b939de72b458461598843f12
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-07-08 18:06:51 +0200

    Fix the make_release script

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 new_version=${1}
-version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' login_ng/Cargo.toml)
+version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' Cargo.toml)
 find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
 #sed -i "s/\"$version\"/\"$new_version\"/g" Cargo.lock
 cargo build
