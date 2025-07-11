bdc266bd440345fa772a48c58ad8869151398937
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-07-11 14:42:57 +0200

    Fix make_release.sh script

--- make_release.sh
+++ make_release.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
+
+set -e
+
 new_version=${1}
-version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' Cargo.toml)
+version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' Cargo.toml | head -n 1)
+echo "Replacing version $version with $new_version"
 find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
 #sed -i "s/\"$version\"/\"$new_version\"/g" Cargo.lock
 cargo build
