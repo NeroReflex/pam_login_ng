7cf0d5a9bc005725a9b820d3270dc61522f5130e
benato.denis96@gmail.com <benato.denis96@gmail.com>
2025-11-07 10:37:52 +0100

    Fix: only change the first occcurrence

--- make_release.sh
+++ make_release.sh
@@ -5,7 +5,7 @@ set -e
 new_version=${1}
 version=$(grep -oP 'version\s*=\s*"\K[0-9]+\.[0-9]+\.[0-9]+' Cargo.toml | head -n 1)
 echo "Replacing version $version with $new_version"
-find . -name "*.toml"  -type f -exec sed -i "s/\"$version\"/\"$new_version\"/g" {} +
+find . -name "*.toml" -type f -exec sed -i '0,/"'"$version"'"/s//"'"$new_version"'"/' {} +
 #sed -i "s/\"$version\"/\"$new_version\"/g" Cargo.lock
 git add .
 git commit -m "Bump version to $new_version"
